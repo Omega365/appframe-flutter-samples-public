af1adf52ae6a8a6ea5f590a25ab0cf2cfe582ea8
Vilius Vigelis <viliusvig@gmail.com>
2020-05-12 15:36:47 +0300

    Cleanup load indc. sample, improve sample template

--- af.widgets.loadingindicator/main.dart
+++ af.widgets.loadingindicator/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
   static const String _title = 'Flutter Code Sample';
@@ -10,30 +8,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sample Code'),
-      ),
-      body: Center(child: 
-        // Construct the building indicator widget
-        AFLoadingIndicator(
-          // Pass your message using the text parameter
-          text: 'Loading...',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Loading Indicator Sample Code'),
+        ),
+        body: Center(child: 
+          // Construct the building indicator widget
+          AFLoadingIndicator(
+            // Pass your message using the text parameter
+            text: 'Loading...',
+          ),
         ),
       ),
     );
--- af.widgets.sampletemplate/main.dart
+++ af.widgets.sampletemplate/main.dart
@@ -1,1 +1,16 @@
-main() => print("Hello, World!");
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Loading Indicator Sample Code'),
+        ),
+        body: // BUILD YOUR WIDGET HERE
+      ),
+    );
+  }
+}
