f1a31150bf77d14efb6af7b035f8e6a2ea60e7bb
Vilius Vigelis <viliusvig@gmail.com>
2020-05-12 11:39:21 +0300

    Fixes loading indc sample

--- af.widgets.loadingindicator/main.dart
+++ af.widgets.loadingindicator/main.dart
@@ -37,3 +37,87 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     );
   }
 }
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+      ),
+      body: Center(child: 
+        AFLoadingIndicator(
+          'Loading',
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+// Ignore the code below, it's required to make the example above
+class AFLoadingIndicator extends StatelessWidget {
+  final String _text;
+
+  AFLoadingIndicator({@required text}) : assert(text != null), _text = text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: FractionalOffset.center,
+      child: new Container(
+        decoration: new BoxDecoration(
+            color: Color(0xFF004A98),
+            borderRadius: new BorderRadius.all(const Radius.circular(40.0))),
+        width: 200,
+        height: 200,
+        child: new Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        Color(0xFFF18A00),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 25),
+                  Text("$_text",
+                      style: Theme.of(context)
+                          .textTheme
+                          .subtitle2
+                          .apply(color: Colors.white))
+                ])),
+      ),
+    );
+  }
+}
